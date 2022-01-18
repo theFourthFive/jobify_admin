0ffd2ef64d7434e13ba5d9f9a0e35d2237fd6727
Achraf Karmous <achraf.karmous@gmail.com>
2022-01-18 22:54:05 +0100

    updated the material-ui v4 to v5 & added a notification icon

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./topbar.css";
 
+// icon from:
+// https://mui.com/components/material-icons/?query=notification
+import { NotificationsNone } from "@mui/icons-material/";
+
 export default function Topbar() {
   return (
     <div className="topbar">
@@ -8,7 +12,11 @@ export default function Topbar() {
         <div className="topLeft">
           <span className="logo">Jobify Admin</span>
         </div>
-        <div className="topRight">Right</div>
+        <div className="topRight">
+          <div className="topbarIcons">
+            <NotificationsNone />
+          </div>
+        </div>
       </div>
     </div>
   );
