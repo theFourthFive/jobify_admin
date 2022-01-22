f4753d46c5d54048fe67699517371b8b11ccd975
Achraf Karmous <achraf.karmous@gmail.com>
2022-01-22 11:49:25 +0100

    Fixed the display of Business Field + it's typo

--- src/pages/companyList/getColumns.jsx
+++ src/pages/companyList/getColumns.jsx
@@ -1,5 +1,5 @@
 import showLabel from "./utils/showLabel";
-import showBussinessField from "./utils/showBussinessField";
+import showBusinessField from "./utils/showBusinessField";
 import showMoreInfo from "./utils/showMoreInfo";
 import showBanButton from "./utils/showBanButton";
 
@@ -8,7 +8,7 @@ export default function getColumns (state, setState) {
   return [
     { field: "id", headerName: "ID", width: 100 },
     { field: "label", headerName: "Label", width: 300, renderCell: showLabel },
-    { field: "bussinessField", headerName: "Bussiness Field", width: 160, renderCell: showBussinessField },
+    { field: "bussinessField", headerName: "Business Field", width: 160, renderCell: showBusinessField },
     { field: "email", headerName: "E-mail", width: 280 },
     { field: "action", headerName: "More Info", headerAlign: 'center', width: 145, renderCell: showMoreInfo },
     { field: "ban", headerName: "Ban / Unban", headerAlign: 'center', width: 160, renderCell: (params) => showBanButton(params, state, setState) },
